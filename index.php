c44157c2a6da27933b267bb21c5404267390e9a9
Pisquano68 <wowchebrutto@gmail.com>
2025-02-28 15:53:29 +0100

    aggiunti requisiti MAILSTON 3

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
         <input type="text" name="name" id="name"> <br>
         <label for="pass">Lunghezza password</label>
         <input type="number" name="pass" id="pass"> <br>
+
+        <input type="checkbox" name="lowercase" id="lowercase">
+        <label for="lowercase">Lettere minuscole</label> <br>
+        <input type="checkbox" name="uppercase" id="uppercase">
+        <label for="uppercase">Lettere maiuscole</label> <br>
+        <input type="checkbox" name="numbers" id="numbers">
+        <label for="numbers">Numeri</label> <br>
+        <input type="checkbox" name="symbols" id="symbols">
+        <label for="symbols">Simboli</label> <br>
             
         <button id="generate">Genera password</button>
     </form>
