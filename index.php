8b6bc22e655fcedd3ab267383b6a861b172f68cd
GiuliaSole <aldegherigiuliasole@gmail.com>
2025-02-28 16:35:53 +0100

    aggiunta css login

--- index.php
+++ index.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="password.php" method="POST">
+<center>  
+<div class="titolino">
+    <h3>Non hai voglia di pensare ad una nuova password?</h3>
+    <h2>TRANQUILLO,</h2>
+    <h3>ci pensiamo noi!!!!</h3>
+</div>      
+<form action="password.php" method="POST" class="login">
         <label for="name">Nome</label>
         <input type="text" name="name" id="name"> <br>
         <label for="pass">Lunghezza password</label>
         <input type="number" name="pass" id="pass"> <br>
             
-        <button id="generate">Genera password</button>
+        <button id="generate" class="bottone">Genera password</button>
     </form>
+</center>
 </body>
 </html>
